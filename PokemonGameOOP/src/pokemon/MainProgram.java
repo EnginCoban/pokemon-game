41bc8b3b71a60bc7160998ec7adcd171c8e34db6
EnginCoban <141469133+EnginCoban@users.noreply.github.com>
2023-08-08 03:08:13 +0200

    Update MainProgram.java

--- PokemonGameOOP/src/pokemon/MainProgram.java
+++ PokemonGameOOP/src/pokemon/MainProgram.java
@@ -3,6 +3,12 @@ package pokemon;
 public class MainProgram {
 
 	public static void main(String[] args) {
+		
+		// initializing the health of every pokemon and the data of the person.
+  		// by executing the try-catch block, it is checked if the given object is the right one.
+		// If yes, the shown method is executed, which will lead to executing other methods till the program is over.
+		// If no, an exception is caught which will display a message to the user.
+		
 		Bisasam bisasam = new Bisasam(100);
 		Glumanda glumanda = new Glumanda(100);
 		Shiggy shiggy = new Shiggy(100);
